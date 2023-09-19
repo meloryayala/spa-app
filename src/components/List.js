8447352656703fed8f99bf55c5fb3bb76e37533a
Melory Ayala <melory.ayala@gmail.com>
2023-09-19 16:01:44 +0200

    Consuming api data

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,27 @@
+import Character from "./Character";
+import {useEffect, useState} from "react";
+
 const List = () => {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const data = await fetch('https://rickandmortyapi.com/api/character');
+
+            const {results} = await data.json;
+            setCharacter(results);
+        }
+
+        fetchData();
+
+    }, [characters.length])
+
     return (
+        <div>
         <h2>Characters</h2>
+            <Character />
+            <Character />
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import ReactDom from 'react-dom/client'
+import List from "./components/List";
 
 function App() {
     return(
+        <div>
         <h1>Ricky and Morty</h1>
+            <List />
+        </div>
     )
 }
 
