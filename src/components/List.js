3247821ed46148d3be552a3b073e79cc2c26066a
Melory Ayala <melory.ayala@gmail.com>
2023-09-19 17:02:54 +0200

    Displaying characters

--- src/components/List.js
+++ src/components/List.js
@@ -2,14 +2,17 @@ import Character from "./Character";
 import {useEffect, useState} from "react";
 
 const List = () => {
+    const [loading, setLoading] = useState(true);
     const [characters, setCharacters] = useState([]);
 
     useEffect(() => {
         async function fetchData(){
             const data = await fetch('https://rickandmortyapi.com/api/character');
 
-            const {results} = await data.json;
-            setCharacter(results);
+            const { results } = await data.json();
+            setCharacters(results);
+            setLoading(false);
+        console.log(data)
         }
 
         fetchData();
@@ -19,10 +22,22 @@ const List = () => {
     return (
         <div>
         <h2>Characters</h2>
-            <Character />
-            <Character />
+            {
+                loading
+                    ? <div>Loading...</div>
+                     : (
+                            characters.map((character) => (
+                            <Character
+                                key={character.id}
+                                name={character.name}
+                                origin={character.origin}
+                                image={character.image}
+                            />
+                            ))
+                    )
+            }
         </div>
-    )
+    );
 }
 
 export default List;
